e2d6c441aafe1d140527e7ea927aa508fa7a37bf
BitByLogic <33465177+BitByLogics@users.noreply.github.com>
2024-07-11 06:30:03 -0400

    improve ModulesCommand

--- Spigot/src/main/java/net/bitbylogic/apibylogic/module/command/ModulesCommand.java
+++ Spigot/src/main/java/net/bitbylogic/apibylogic/module/command/ModulesCommand.java
@@ -1,6 +1,8 @@
 package net.bitbylogic.apibylogic.module.command;
 
 import net.bitbylogic.apibylogic.module.Module;
+import net.bitbylogic.apibylogic.module.ModuleData;
+import net.bitbylogic.apibylogic.module.ModuleInterface;
 import net.bitbylogic.apibylogic.module.ModuleManager;
 import net.bitbylogic.apibylogic.util.NumberUtil;
 import net.bitbylogic.apibylogic.util.message.Formatter;
@@ -8,10 +10,17 @@ import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.hover.content.Text;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.util.StringUtil;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ModulesCommand extends ModuleCommand {
 
@@ -32,6 +41,7 @@ public class ModulesCommand extends ModuleCommand {
             sender.sendMessage(Formatter.command("module reload <id>", "Reload the specified module's config."));
             sender.sendMessage(Formatter.command("module enable <id>", "Enable the specified module."));
             sender.sendMessage(Formatter.command("module disable <id>", "Disable the specified module."));
+            sender.sendMessage(Formatter.command("module toggle <id>", "Toggles the specified module."));
             return;
         }
 
@@ -115,6 +125,32 @@ public class ModulesCommand extends ModuleCommand {
 
             sender.sendMessage(Formatter.success("Modules", "Disabling module! <c#separator>(<c#success_secondary>Name<c#separator>: %s<c#separator>, <c#success_secondary>ID<c#separator>: %s<c#separator>)", module.getModuleData().getName(), module.getModuleData().getId()));
             moduleManager.disableModule(module.getModuleData().getId());
+            return;
+        }
+
+        if (args[0].equalsIgnoreCase("toggle")) {
+            if (args.length < 2) {
+                sender.sendMessage(Formatter.error("Modules", "Invalid module ID."));
+                return;
+            }
+
+            Module module = moduleManager.getModuleByID(args[1]);
+
+            if (module == null) {
+                sender.sendMessage(Formatter.error("Modules", "Invalid module."));
+                return;
+            }
+
+            if (module.isEnabled()) {
+                module.setEnabled(false);
+                sender.sendMessage(Formatter.success("Modules", "Disabling module! <c#separator>(<c#success_secondary>Name<c#separator>: %s<c#separator>, <c#success_secondary>ID<c#separator>: %s<c#separator>)", module.getModuleData().getName(), module.getModuleData().getId()));
+                moduleManager.disableModule(module.getModuleData().getId());
+                return;
+            }
+
+            module.setEnabled(true);
+            sender.sendMessage(Formatter.success("Modules", "Enabling module! <c#separator>(<c#success_secondary>Name<c#separator>: %s<c#separator>, <c#success_secondary>ID<c#separator>: %s<c#separator>)", module.getModuleData().getName(), module.getModuleData().getId()));
+            moduleManager.enableModule(module.getModuleData().getId());
         }
     }
 
@@ -143,7 +179,7 @@ public class ModulesCommand extends ModuleCommand {
                     Formatter.format(module.isEnabled() ? "<c#success_highlight>Enabled" : "<c#error_highlight>Disabled"));
 
             moduleComponent.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text(Formatter.richFormat("<c#highlight>%s", module.getModuleData().getDescription()))));
-            moduleComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, module.isEnabled() ? "/module disable " + module.getModuleData().getId() : "/module enable " + module.getModuleData().getId()));
+            moduleComponent.setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/module toggle " + module.getModuleData().getId()));
 
             sender.spigot().sendMessage(moduleComponent);
         }
@@ -151,4 +187,21 @@ public class ModulesCommand extends ModuleCommand {
         sender.sendMessage(Formatter.replace("<c#separator>&m        &r <c#separator>( <c#secondary>Page<c#separator>: <c#highlight>%s <c#separator>)&m        ", page));
     }
 
+    @Override
+    public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
+        if (args.length == 1) {
+            return StringUtil.copyPartialMatches(args[0], Arrays.asList("list", "reload", "enable", "disable"), new ArrayList<>());
+        }
+
+        if (args.length != 2) {
+            return new ArrayList<>();
+        }
+
+        if (args[0].equalsIgnoreCase("list")) {
+            return StringUtil.copyPartialMatches(args[1], Collections.singleton("1"), new ArrayList<>());
+        }
+
+        return StringUtil.copyPartialMatches(args[1], moduleManager.getModules().values().stream()
+                .map(ModuleInterface::getModuleData).map(ModuleData::getId).collect(Collectors.toSet()), new ArrayList<>());
+    }
 }
