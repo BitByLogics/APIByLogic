9799c58ab09e5c95c99aad4fc4c8355cdd13c754
BitByLogic <33465177+BitByLogics@users.noreply.github.com>
2024-07-11 02:55:39 -0400

    remove plugin field for MetricsWrapper

--- Spigot/src/main/java/net/bitbylogic/apibylogic/metrics/MetricsWrapper.java
+++ Spigot/src/main/java/net/bitbylogic/apibylogic/metrics/MetricsWrapper.java
@@ -6,17 +6,15 @@ import org.bstats.charts.SimplePie;
 
 public class MetricsWrapper {
 
-    private final APIByLogic plugin;
     private final Metrics metrics;
 
     public MetricsWrapper(APIByLogic plugin) {
-        this.plugin = plugin;
         this.metrics = new Metrics(plugin, 21738);
 
-        addDatabaseCharts();
+        addDatabaseCharts(plugin);
     }
 
-    private void addDatabaseCharts() {
+    private void addDatabaseCharts(APIByLogic plugin) {
         metrics.addCustomChart(new SimplePie("using_redis", () -> plugin.getRedisManager() == null ? "false" : "true"));
         metrics.addCustomChart(new SimplePie("using_hikari", () -> plugin.getHikariAPI() == null ? "false" : "true"));
     }
